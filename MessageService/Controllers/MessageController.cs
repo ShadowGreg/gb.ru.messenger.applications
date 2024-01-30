8427337532084ff6ba4f7267f0a4ff12c77923f8
Greg Osetrov <shadow-greg@yandex.ru>
2024-01-30 14:32:31 +0300

    добавил обработку авторизованных пользователей

--- MessageService/Controllers/MessageController.cs
+++ MessageService/Controllers/MessageController.cs
@@ -1,4 +1,6 @@
-﻿using MessageDataBase.Repository;
+﻿using DataBase;
+using DataBase.Repository;
+using MessageDataBase.Repository;
 using MessageService.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +11,7 @@ namespace MessageService.Controllers;
 public class MessageController: ControllerBase {
     private readonly IConfiguration _configuration;
     private readonly IMessageRepository _messageRepository;
+    private UserContext _userRepository;
 
     public MessageController(IConfiguration configuration, IMessageRepository messageRepository) {
         _configuration = configuration;
@@ -25,11 +28,22 @@ public class MessageController: ControllerBase {
     [HttpPost]
     [Route("SendMessage")]
     public IActionResult SendMessage(MessageDTO message) {
-        var messageId = _messageRepository.SendMessage(
-            message.Text,
-            message.SenderName,
-            message.ReceiverName
-        );
-        return Ok(messageId);
+        _userRepository = new UserContext();
+        var sender = _userRepository
+            .Users
+            .FirstOrDefault(sender => sender.Name == message.SenderName);
+        var recever = _userRepository
+            .Users
+            .FirstOrDefault(reciver => reciver.Name == message.ReceiverName);
+        if (sender!=null && recever!=null) {
+            var messageId = _messageRepository.SendMessage(
+                message.Text,
+                message.SenderName,
+                message.ReceiverName
+            );
+            return Ok(messageId);
+        }
+
+        return NotFound("Sender or Receiver not fount in base");
     }
 }
